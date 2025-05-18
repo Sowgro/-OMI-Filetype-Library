0be8ba7ff3e28cb94fa712a78849e1595f2121d0
sowgro <tpoke.ferrari@gmail.com>
2025-05-18 17:02:03 -0400

    Fix dependencies

--- OMI Filetypes Library/Formats/GameRuleFile.cs
+++ OMI Filetypes Library/Formats/GameRuleFile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO.Compression;
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 using OMI.Workers.GameRule;
 
 namespace OMI.Formats.GameRule
